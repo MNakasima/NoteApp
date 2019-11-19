6f8a297d848b61c669072e30fb79e703db162754
Maruko <marco.k95@hotmail.com>
2019-11-19 14:14:33 -0300

    introduction to sqlite

--- app/src/main/java/com/mnakasima/note/AddNotes.kt
+++ app/src/main/java/com/mnakasima/note/AddNotes.kt
@@ -1,8 +1,11 @@
 package com.mnakasima.note
 
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_add_notes.*
 
 class AddNotes : AppCompatActivity() {
 
@@ -12,6 +15,21 @@ class AddNotes : AppCompatActivity() {
     }
 
     fun buAdd(view: View){
-        finish()
+
+        var dbManager = DbManager(this)
+
+        var values = ContentValues()
+        values.put("Title", etTitle.text.toString())
+        values.put("Description", etDes.text.toString())
+
+        val ID = dbManager.Insert(values)
+
+        if(ID>0){
+            Toast.makeText(this,"Note added", Toast.LENGTH_LONG).show()
+            finish()
+        }else{
+            Toast.makeText(this,"Cannot Add Note", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
